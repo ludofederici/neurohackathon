6c29edecc2273f3c7b6a0103c12d9a39c55e0c4f
Ludovico Federici <ludofederici@berkeley.edu>
2024-04-06 19:08:21 -0700

    calculating average focus score over period of time

--- src/pages/Focus.js
+++ src/pages/Focus.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { navigate } from "@reach/router";
 
-
 import { notion, useNotion } from "../services/notion";
 import { Nav } from "../components/Nav";
 
 export function Focus() {
   const { user } = useNotion();
   const [focus, setFocus] = useState(0);
+  const [color, setColor] = useState('');
+  const [focusScores, setFocusScores] = useState([]); // Step 1: Initialize state for focus scores list
+  const [averageFocusScore, setAverageFocusScore] = useState(0);
 
   useEffect(() => {
     if (!user) {
@@ -23,6 +25,23 @@ export function Focus() {
     const subscription = notion.focus().subscribe((focus) => {
       const focusScore = Math.trunc(focus.probability * 100);
       setFocus(focusScore);
+
+      // Append the new focus score to the focusScores list
+      setFocusScores((prevScores) => {
+      const newFocusScores = [...prevScores, focusScore];
+
+      // Calculate the average using the new array
+      const averageFocusScore = Math.round(newFocusScores.slice(-20).reduce((acc, score) => acc + score, 0) / Math.min(newFocusScores.length, 20));
+      setAverageFocusScore(averageFocusScore);
+
+      return newFocusScores;
+    });
+
+      if (focusScore < 30) {
+        setColor('red');
+      } else {
+        setColor('green');
+      }
     });
 
     return () => {
@@ -33,9 +52,10 @@ export function Focus() {
   return (
     <main className="main-container">
       {user ? <Nav /> : null}
-      <div className="focus-score">
-        &nbsp;{focus}% <div className="focus-word">Focus</div>
+      <div className="metric-score" style={{ borderColor: color }}>
+        &nbsp;{focus}% <div className="metric-word">Focus</div>
       </div>
+      <div>Average of last 20 scores: {averageFocusScore}%</div>
     </main>
   );
 }
